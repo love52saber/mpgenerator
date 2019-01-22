969b35311a712599ed193258354a5e6534fcc3e2
love52saber <1457659650@qq.com>
2019-01-22 22:23:14 +0800

    修改

--- src/main/java/com/hedian/mpgenerator/Mp3.java
+++ src/main/java/com/hedian/mpgenerator/Mp3.java
@@ -1,14 +1,18 @@
 package com.hedian.mpgenerator;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
 import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mp3 {
@@ -35,15 +39,60 @@ public class Mp3 {
         // 代码生成器
         AutoGenerator mpg = new AutoGenerator();
 
-        // 全局配置
+        // 1数据源配置
+        //1.1mysql
+//        DataSourceConfig dsc = new DataSourceConfig();
+//        dsc.setUrl("jdbc:mysql://localhost:3306/ant?useUnicode=true&useSSL=false&characterEncoding=utf8");
+//        // dsc.setSchemaName("public");
+//        dsc.setDriverName("com.mysql.jdbc.Driver");
+//        dsc.setUsername("root");
+//        dsc.setPassword("密码");
+//        mpg.setDataSource(dsc);
+        //1.2oracle
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:oracle:thin:@47.96.150.182:1521:akydb");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("oracle.jdbc.driver.OracleDriver");
+        dsc.setUsername("jsasst");
+        dsc.setPassword("jsasst2018");
+        mpg.setDataSource(dsc);
+
+        // 2数据库表设置 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setLogicDeleteFieldName("USEABLE");
+
+        ArrayList<TableFill> tableFillList = new ArrayList<>();
+        tableFillList.add(new TableFill("CREATE_TIME", FieldFill.INSERT));
+        tableFillList.add(new TableFill("USEABLE", FieldFill.INSERT));
+        tableFillList.add(new TableFill("MODIFY_TIME", FieldFill.UPDATE));
+        tableFillList.add(new TableFill("TANK_AREA_ID", FieldFill.INSERT));
+        strategy.setTableFillList(tableFillList);
+
+//        strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+//        strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
+//        strategy.setInclude(new String[]{"FW_AUTH_WS_INFO","FW_AUTH_WS_DD","FW_AUTH_DD_DC_DETAIL","FW_AUTH_DD",
+//                "FW_AUTH_WS_DC_DD_OPT", "FW_AUTH_DC_DD_INVOLVE_OPT"});
+        strategy.setInclude(new String[]{"FW_AUTH_TANK"});
+        strategy.setSuperEntityColumns("id");
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix("FW_AUTH");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+
+        // 3全局配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
         gc.setOutputDir("D://");
         gc.setAuthor("gjyang");
         gc.setOpen(true);        //完成后打开文件夹
         gc.setFileOverride(true);
-        gc.setDateType(DateType.ONLY_DATE);
-
+        gc.setDateType(DateType.ONLY_DATE);//数据库日期转换成什么格式Date或localDate
+        gc.setIdType(IdType.UUID);
+        gc.setSwagger2(true);//开启swagger2注解
         // 自定义文件命名，注意 %s 会自动填充表实体属性！
         // gc.setMapperName("%sDao");
         // gc.setXmlName("%sDao");
@@ -55,25 +104,6 @@ public class Mp3 {
         gc.setBaseColumnList(true);//xml文件生成baseresult
 
         mpg.setGlobalConfig(gc);
-
-        // 数据源配置
-//        DataSourceConfig dsc = new DataSourceConfig();
-//        dsc.setUrl("jdbc:mysql://localhost:3306/ant?useUnicode=true&useSSL=false&characterEncoding=utf8");
-//        // dsc.setSchemaName("public");
-//        dsc.setDriverName("com.mysql.jdbc.Driver");
-//        dsc.setUsername("root");
-//        dsc.setPassword("密码");
-//        mpg.setDataSource(dsc);
-
-        //oracle
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:oracle:thin:@47.96.150.182:1521:akydb");
-        // dsc.setSchemaName("public");
-        dsc.setDriverName("oracle.jdbc.driver.OracleDriver");
-        dsc.setUsername("jsasst");
-        dsc.setPassword("jsasst2018");
-
-        mpg.setDataSource(dsc);
 
         // 包配置
         PackageConfig pc = new PackageConfig();
@@ -120,22 +150,6 @@ public class Mp3 {
 //        templateConfig.setXml(null);
         mpg.setTemplate(templateConfig);
 
-        // 策略配置
-        StrategyConfig strategy = new StrategyConfig();
-        strategy.setNaming(NamingStrategy.underline_to_camel);
-        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-//        strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
-        strategy.setEntityLombokModel(true);
-        strategy.setRestControllerStyle(true);
-//        strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
-//        strategy.setInclude(new String[]{"FW_AUTH_WS_INFO","FW_AUTH_WS_DD","FW_AUTH_DD_DC_DETAIL","FW_AUTH_DD",
-//                "FW_AUTH_WS_DC_DD_OPT", "FW_AUTH_DC_DD_INVOLVE_OPT"});
-        strategy.setInclude(new String[]{"FW_AUTH_WS_DC_DD_OPT"});
-        strategy.setSuperEntityColumns("id");
-        strategy.setControllerMappingHyphenStyle(true);
-        strategy.setTablePrefix("FW_AUTH");
-        mpg.setStrategy(strategy);
-        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
         mpg.execute();
     }
 }
